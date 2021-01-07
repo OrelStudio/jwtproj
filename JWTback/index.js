ef882acdf1927eda38d966df4f02c6b37d5598ff
Orel Shriki <30898660+OrelStudio@users.noreply.github.com>
2021-01-07 17:03:16 +0200

    sdf
    sdfgd

--- JWTback/index.js
+++ JWTback/index.js
@@ -12,11 +12,12 @@ const validate = require('./routes/refresh')
 const internalIp = require('internal-ip');
 
 dotenv.config()
-let ip = '192.168.1.11'
+//let ip = '192.168.1.11'
+const db = "mongodb+srv://orelnya:OrelMan10@cluster0.ag3sz.mongodb.net/app?retryWrites=true&w=majority"
 
 // Connect to DB
 mongoose.connect(
-  process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+  db, { useNewUrlParser: true }, () => {
     console.log('connected to DB')
   }
 )
